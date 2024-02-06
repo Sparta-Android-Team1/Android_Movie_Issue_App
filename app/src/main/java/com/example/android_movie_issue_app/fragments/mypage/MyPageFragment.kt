83530b2d7e1295d82da29a9a7793b5466e9fe496
avo890 <wizhey7327@gmail.com>
2024-02-06 21:03:13 +0900

    [UI] Channel/MyPage/Detail 페이지 UI 구성

--- app/src/main/java/com/example/android_movie_issue_app/fragments/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/android_movie_issue_app/fragments/mypage/MyPageFragment.kt
@@ -28,10 +28,10 @@ class MyPageFragment : Fragment() {
         _binding = FragmentMyPageBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textMyPage
-        myPageViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textMyPage
+//        myPageViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
